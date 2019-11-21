7a4001dbfb076aca800ed5b28aae5a9a09c43505
Ismael Hasan <ismael.hasan@live.com>
2019-11-21 04:08:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,29 @@
 def addition(num1, num2)
-  add = (num1=5) + (num2=4)
-  p add
-
+  add = num1 + num
+  add
 end
 
 def subtraction(num1, num2)
-
+  subtact = num1 - num2
+  subtract
 end
 
 def division(num1, num2)
-
+  div = num / num2
+  div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
+  sq_rt = 
 
 end
